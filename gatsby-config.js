b9f5469fda8d342d9f774fddec26dedf3956994d
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-10-21 23:47:49 -0700

    Update to correct tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,7 +88,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "50926482",
+        trackingId: "UA-25956864-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional

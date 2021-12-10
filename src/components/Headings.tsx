7a8c08851354e5110a1ea0df45610ae40a182919
Samuel Gordalina <samuel.gordalina@gmail.com>
2021-12-10 15:02:22 -0800

    Use prism elixir syntax highlighting

--- src/components/Headings.tsx
+++ src/components/Headings.tsx
@@ -1,48 +1,4 @@
-import React from "react"
 import { Themed } from "theme-ui"
-// import styled from "@emotion/styled"
-// import copy from 'copy-to-clipboard';
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// import { faLink } from "@fortawesome/free-solid-svg-icons"
-// import { slug } from 'github-slugger'
-
-// const heading = (Component) => {
-//   const Wrapper = styled(Component)`
-//     a.anchor {
-//       float: left;
-//       margin-left: -1.25em;
-//       padding-right: 1.25em;
-//       font-size: 0.6em;
-//       width: 0.85em;
-//       cursor: pointer;
-//       visibility: hidden;
-//       color: var(--theme-ui-colors-primary);
-
-//       &:hover {
-//         visibility: visible;
-//       }
-//     }
-
-//     &:hover a.anchor {
-//       visibility: visible;
-//     }
-//   `
-
-//   return ({ children, ...rest }) => {
-//     const id = rest.id || slug(String(children).trim());
-//     const href = `${window.location.href.split('#')[0]}#${id}`;
-
-//     return (
-//       <Wrapper id={id}>
-//         {/* <a aria-hidden={true} className={"anchor"} href={href} onClick={() => copy(href)}>
-//           <FontAwesomeIcon icon={faLink} />
-//         </a> */}
-//         {children}
-//       </Wrapper>
-//     );
-//   }
-// }
-
 
 export const h1 = Themed.h1;
 export const h2 = Themed.h2;
